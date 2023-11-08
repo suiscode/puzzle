324d766a47eb7d1555ad10d3bf9dc57e9cca42bc
suiscode <tuuduu00@gmail.com>
2023-11-09 00:38:57 +0800

    redid

--- app.js
+++ app.js
@@ -1,29 +1,67 @@
-const draggables = document.querySelectorAll('#draggable')
-const containerboxes = document.querySelectorAll('.box')
+const rows = 3;
+const col = 3;
 
-let currentlyDragging = null; // Track the currently dragging element
+let currBox;
+let otherBox;
 
-draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('dragging');
-        currentlyDragging = draggable; 
-    });
+let turns= 0;
 
-    draggable.addEventListener('dragend', () => {
-        draggable.classList.remove('dragging');
-        currentlyDragging = null; 
-    });
-});
-
-containerboxes.forEach(containerbox => {
-    containerbox.addEventListener('dragover', (event) => {
-        event.preventDefault(); 
-
-        if (currentlyDragging) {
-            if (containerbox.querySelector('div') === null) {
-                console.log('hi');
-                containerbox.appendChild(currentlyDragging);
-            }
+const imgOrder = [];
+const randomArrayGen=()=>{
+    while(imgOrder.length<9){
+        const random = Math.floor(Math.random()*9+1)
+        if(!imgOrder.includes(random)){
+            imgOrder.push(random)
         }
-    });
-});
+    }
+}
+randomArrayGen()
+
+window.onload =function(){
+    for(let r= 0; r<rows; r++){
+        for(let c= 0; c<rows; c++){
+            let box = document.createElement("img");
+            box.id= `${r}-${c}`
+            box.src="./" + imgOrder.shift() + '.png'
+
+            box.addEventListener('dragstart', dragStart)
+            box.addEventListener('dragover', dragOver)
+            box.addEventListener('dragenter', dragEnter)
+            box.addEventListener('dragleave', dragLeave)
+            box.addEventListener('drop', drop)
+            box.addEventListener('dragend', dragEnd)
+
+            document.getElementById("container").append(box)
+        }
+    }
+    console.log('ran');
+    
+}
+
+function dragStart(){
+    currBox=this
+}
+
+function dragOver(e){
+    e.preventDefault()
+}
+
+function dragEnter(e){
+    e.preventDefault()
+}
+
+function dragLeave(e){
+    e.preventDefault()
+}
+
+function drop(){
+    otherBox=this
+}
+
+function dragEnd(){
+    let currImg = currBox.src
+    let otherImg = otherBox.src
+
+    currBox.src = otherImg
+    otherBox.src = currImg
+}
